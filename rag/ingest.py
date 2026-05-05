dae57ca884ede19882000a24dfbe9d1f2fef7377
Chirag Dua <chiragdua05@gmail.com>
2026-05-05 22:26:42 +0530

    Step 3: Created FAISS vector database

--- rag/ingest.py
+++ rag/ingest.py
@@ -1,6 +1,14 @@
+from dotenv import load_dotenv
+import os
+
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings
+load_dotenv()
 
+if not os.getenv("OPENAI_API_KEY"):
+    raise ValueError("❌ OPENAI_API_KEY not found in .env file")
 loader = TextLoader("data/tax_docs/tax_info.txt")
 documents=loader.load()
 
@@ -13,6 +21,7 @@ text_splitter = CharacterTextSplitter(
 
 chunks = text_splitter.split_documents(documents)
 
-# 4. Print the result
-for i, chunk in enumerate(chunks):
-    print(f"Chunk {i}: {chunk.page_content}\n")
+embeddings = OpenAIEmbeddings()
+db = FAISS.from_documents(chunks, embeddings)
+db.save_local("faiss_index")
+print("✅ FAISS index created and saved successfully")
