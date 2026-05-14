e1607e0071e28057340849e07cf66dabfd7a3059
Chirag Dua <chiragdua05@gmail.com>
2026-05-15 02:34:48 +0530

    Fix Firebase config import for Streamlit deployment

---
+++ utils/firebase_config.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+firebase_config = {
+    "apiKey": st.secrets["firebase"]["apiKey"],
+    "authDomain": st.secrets["firebase"]["authDomain"],
+    "projectId": st.secrets["firebase"]["projectId"],
+    "storageBucket": st.secrets["firebase"]["storageBucket"],
+    "messagingSenderId": st.secrets["firebase"]["messagingSenderId"],
+    "appId": st.secrets["firebase"]["appId"],
+}
